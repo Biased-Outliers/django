444dcdcbc95f2eec8f6409f8e837c75eb5e15c36
Taraqur <tqrahman8@gmail.com>
2021-12-26 14:38:31 -0500

    removed vitural env

--- sabisands/homeApp/views.py
+++ sabisands/homeApp/views.py
@@ -5,12 +5,12 @@ from .forms import ImageForm
 
 # Create your views here.
 def hello(request):
-    # text = '''<h1>Welcome to Sabi Sands!</h1>'''
     if request.method=='POST':
         form = ImageForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return render(request, 'homeApp/index.html', {'result':'YES'})
+            img_obj = form.instance
+            return render(request, 'homeApp/index.html', {'image':img_obj})
     form = ImageForm()
     return render(request, 'homeApp/index.html', {
         'form':form
